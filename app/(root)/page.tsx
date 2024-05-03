69f2002af102a2985bae1c40264a568a94eae183
Med Amine <ma.elazzab@gmail.com>
2024-05-03 16:23:25 +0100

    Add new components and update layout in root files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,39 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const loggedIn = {
+    firstName: "Med Amine",
+    lastName: "Elazzab",
+    email: "ma.elazzab@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="This is a simple app to help you get started with your project"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1230.0}
+          />
+        </header>
+        RECENT
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1233.5 }, { currentBalance: 2343.5 }]}
+      />
+    </section>
+  );
+};
 
 export default Home;
